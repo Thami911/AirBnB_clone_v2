51c457f5bc367508103cb367ee52725f07521d85
Thami911 <122853011+Thami911@users.noreply.github.com>
2023-10-22 15:32:29 +0200

    Update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-from flask import Flask, render_template
-from models import *
+"""Start web application with two routings"""
 from models import storage
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+@app.route('/cities_by_states')
+def states_list():
+    """Render template with states"""
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+     return render_template(path, states=states)
 
 @app.teardown_appcontext
-def app_teardown(exception):
-    """closes the storage on teardown"""
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
-    
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
